2b914a3333a5b5809e069cbab097f08838789903
Brandon Holland <brandon@spydercontrols.com>
2025-12-22 09:24:51 -0800

    Fixed incorrect code for ifdef

--- src/draw/lv_draw_buf.c
+++ src/draw/lv_draw_buf.c
@@ -520,10 +520,10 @@ lv_result_t lv_draw_buf_from_image(lv_draw_buf_t * buf, const lv_image_dsc_t * i
 {
 #ifdef FW_ETHERNET_BUILD
     const lv_result_t res = lv_draw_buf_init(buf, img->header.w, img->header.h, img->header.cf, img->header.stride,
-                                             (void *)img->data, img->data_size);
+    										 GUI_DATA_OFFSET_PTR((void *)img->data), img->data_size);
 #else
     const lv_result_t res = lv_draw_buf_init(buf, img->header.w, img->header.h, img->header.cf, img->header.stride,
-    										 GUI_DATA_OFFSET_PTR((void *)img->data), img->data_size);
+                                             (void *)img->data, img->data_size);
 #endif
     if(res != LV_RESULT_OK) {
         return res;
