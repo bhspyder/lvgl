d428958bbffb883ebb838c5abaef4bb7cb4b3157
Brandon Holland <brandon@spydercontrols.com>
2026-05-08 10:33:55 -0700

    Added fix to allow a screen object to add/remove LV_OBJ_FLAG_HIDDEN

--- src/core/lv_obj.c
+++ src/core/lv_obj.c
@@ -278,7 +278,8 @@ void lv_obj_add_flag(lv_obj_t * obj, lv_obj_flag_t f)
     }
 
     if((was_on_layout != lv_obj_is_layout_positioned(obj)) || (f & (LV_OBJ_FLAG_LAYOUT_1 |  LV_OBJ_FLAG_LAYOUT_2))) {
-        lv_obj_mark_layout_as_dirty(lv_obj_get_parent(obj));
+    	if (obj->parent != NULL)
+    		lv_obj_mark_layout_as_dirty(obj->parent);
         lv_obj_mark_layout_as_dirty(obj);
     }
 
@@ -308,12 +309,14 @@ void lv_obj_remove_flag(lv_obj_t * obj, lv_obj_flag_t f)
 
     if(f & LV_OBJ_FLAG_HIDDEN) {
         lv_obj_invalidate(obj);
-        lv_obj_mark_layout_as_dirty(lv_obj_get_parent(obj));
+        if (obj->parent != NULL)
+        	lv_obj_mark_layout_as_dirty(obj->parent);
         lv_obj_mark_layout_as_dirty(obj);
     }
 
     if((was_on_layout != lv_obj_is_layout_positioned(obj)) || (f & (LV_OBJ_FLAG_LAYOUT_1 |  LV_OBJ_FLAG_LAYOUT_2))) {
-        lv_obj_mark_layout_as_dirty(lv_obj_get_parent(obj));
+    	if (obj->parent != NULL)
+    		lv_obj_mark_layout_as_dirty(obj->parent);
     }
 
 }
